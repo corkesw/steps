c1c4bcb8eb555b2475ff2f605caefe5b3dbe506c
corkesw <home@stephencorke.com>
2022-02-13 13:35:24 +0000

    add heroku

--- db/data/data.js
+++ db/data/data.js
@@ -1,12 +1,39 @@
 exports.data = [
   {
-    startDate: new Date(2021, 1, 1),
-    endDate: new Date(2021, 1, 31),
+    month: 1,
+    year: 2021,
+    days: 31,
+    steps: 100000,
+  },
+  {
+    month: 2,
+    year: 2021,
+    days: 28,
+    steps: 300000,
+  },
+  {
+    month: 4,
+    year: 2021,
+    days: 31,
+    steps: 70000,
+  },
+  {
+    month: 3,
+    year: 2021,
+    days: 30,
     steps: 200000,
   },
   {
-    startDate: new Date(2021, 2, 1),
-    endDate: new Date(2021, 2, 31),
-    steps: 150000,
+    month: 5,
+    year: 2021,
+    days: 31,
+    steps: 200000,
+  },
+  {
+    month: 6,
+    year: 2022,
+    days: 30,
+    steps: 200000,
   },
 ];
+
